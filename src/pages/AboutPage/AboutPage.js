5dc77cb29dc3965ff036b9371459fd03be2cb3bb
Dylan Lovell <Dylovell@live.com>
2018-07-31 08:52:56 -0600

    first push

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ScrollToNext from '@components/ScrollToNext';
+import './style.scss';
+
+const AboutPage = (props, context) => {
+  const {
+    theme: { colorPrimary, colorHighlight, bgPrimary, textPrimary }
+  } = context;
+
+  return (
+    <div className="about-page" style={{ backgroundColor: bgPrimary }}>
+      <style jsx="true">
+        {`
+          .highlight {
+            background-color: ${colorHighlight};
+          }
+          ::selection {
+            background-color: ${colorHighlight};
+          }
+        `}
+      </style>
+      <div className="content-grid">
+        <h1 style={{ color: colorPrimary }}>About</h1>
+        <div className="about-wrapper">
+          <div className="about-content" style={{ color: textPrimary }}>
+            <p>
+            I am extremely motivated to find solutions to problems. 
+            </p>
+            <p>
+              I love learning how everything works, whether it is a Web Technology, astrophysics, or a manufacturing process.  
+            </p>
+            <p>
+            <span className="highlight">JavaScript</span> , React-Redux , <span className="highlight">Node.js</span> , Express , <span className="highlight">HTML/CSS</span> , and much more!
+            </p>
+            <p>
+              I was born curious about the world. From a young age I had a deep interest in making and building. From Lego pinball machines to discovering basic circuitry using aluminum foil and duck tape, I never wanted to stop learning. Thankfully that fire never went out, and I am still learning and making new things all the time.  
+            </p>
+            <p>
+              Spending almost four years in IT, troubleshooting and problem solving are some of my strong suits. Some of the projects that I worked on while working in IT involved Virtual Desktop Infrastructure testing and deployment, network upgrades, server upgrades, and the constant task of getting the correct equipment to employees to help enable them to do their jobs. Finding the right tool to help the employee succeed was one of the most fulfilling parts of my job.  
+            </p>
+            <p>
+My exposure to IT was extremally helpful for the next step, Web Development. The opportunity to attend a Web Development Bootcamp, called Dev Mountain, arose and I jumped on the opportunity.  I have loved How much this bootcamp and set a foundation to build on, and the tool set it gave me to make/learn to make anything I want! 
+            </p>
+            <p>
+              Email me for any inquries  <span className="highlight">Dylovell@live.com</span>
+            </p>
+            <p className="text-emoji" style={{ color: colorPrimary }}>
+              \ (•◡•) /
+            </p>
+          </div>
+        </div>
+      </div>
+      <ScrollToNext pageSelector=".portfolio-page" />
+    </div>
+  );
+};
+
+AboutPage.contextTypes = {
+  theme: PropTypes.any
+};
+
+export default AboutPage;
---
+++ src/pages/PortfolioPage/portfolio-items.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default [{
+  name: 'typy',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        oSlash WIFI Analyser
+      </div>
+      <div className='portfolio-item__desc'>
+        A mobile site helping you find the best WIFI channel for you, in your area.
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-react"></i>
+        <i className="fab fa-js"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Dylovell/Oslash-Wifi">Code</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://oSlash.info">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'Kwizz',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        Kwizz
+      </div>
+      <div className='portfolio-item__desc'>
+        A web app for making quizes and taking with three or more players.
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-react"></i>
+        <i className="fab fa-js"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Kahoot-Clone/kahoot-clone">Code</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Kahoot-Clone/kahoot-clone">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'npm-module-boilerplate',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        NPM Module Boilerplate
+      </div>
+      <div className='portfolio-item__desc'>
+         Boilerplate plate code to create and publish an NPM package.
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-js"></i>
+        <i className="fab fa-npm"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://dev.to/flexdinesh/publish-your-own-npm-package---5b71">Blog</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/flexdinesh/npm-module-boilerplate">More</a>
+      </div>
+    </div>
+  )
+}]
